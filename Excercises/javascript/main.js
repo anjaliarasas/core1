d2aaebf2bf851b0a0ff4b7afd34bd44893044202
anjaliarasas <113141493+anjaliarasas@users.noreply.github.com>
2023-01-27 10:35:55 -0500

    HI

--- Excercises/javascript/main.js
+++
@@ -1,8 +0,0 @@
-const audio = new Audio("assets/click.mp3");
-const buttons = document.querySelectorAll("button")
-
-buttons.forEach(button => {
-  button.addEventListener ("click", () => {
-    audio.play();
-  });
-});
